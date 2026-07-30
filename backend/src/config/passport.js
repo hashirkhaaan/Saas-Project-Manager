8b00ba76e541f6dbed9681943878d44f028b4a1e
hashirkhaaan <grammarkgl123@gmail.com>
2026-07-30 22:19:05 +0500

    Setup oauth for google authentication

---
+++ backend/src/config/passport.js
@@ -0,0 +1,43 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { User } from "../models/user.model.js";
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: process.env.GOOGLE_CALLBACK_URL,
+        },
+        async (accessToken, refreshToken, profile, done) => {
+            try {
+                let user = await User.findOne({ googleId: profile.id });
+
+                if (!user) {
+                    user = await User.findOne({
+                        email: profile.emails[0].value,
+                    });
+                    if (user) {
+                        user.googleId = profile.id;
+                        await user.save({ validateBeforeSave: false });
+                    } else {
+                        user = await User.create({
+                            googleId: profile.id,
+                            fullName: profile.displayName,
+                            username: profile.emails[0].value
+                                .split("@")[0]
+                                .replace(/[^a-zA-Z0-9]/g, ""),
+                            email: profile.emails[0].value,
+                            avatar: profile.photos[0].value,
+                        });
+                    }
+                }
+                return done(null, user);
+            } catch (error) {
+                done(error, null);
+            }
+        }
+    )
+);
+
+export default passport;
