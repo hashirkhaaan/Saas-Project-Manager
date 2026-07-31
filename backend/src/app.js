8251a2e733e97b5b692cd4b78b612524f9476189
hashirkhaaan <grammarkgl123@gmail.com>
2026-07-31 22:02:23 +0500

    Added configurations for passport package

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,27 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import passport from "./config/passport.js"
+import session from "express-session"
 
 const app = express();
 
+// Configurations for the app
 app.use(
     cors({
         origin: process.env.CORS_ORIGIN,
         credentials: true,
     })
 );
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+    })
+)
+app.use(passport.initialize())
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
