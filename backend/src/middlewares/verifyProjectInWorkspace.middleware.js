f604b116c6e2a3b4d7b120cdf6ce1006fcc85438
hashirkhaaan <grammarkgl123@gmail.com>
2026-08-22 16:59:33 +0500

    Completed the project in workspace verification middleware

--- backend/src/middlewares/verifyProjectInWorkspace.middleware.js
+++ backend/src/middlewares/verifyProjectInWorkspace.middleware.js
@@ -4,7 +4,32 @@ import { isValidObjectId } from "mongoose";
 import { ApiError } from "../utils/ApiError.js";
 
 const verifyProjectInWorkspace = asyncHandler(async (req, res, next) => {
+    try {
+        const { projectId } = req.params;
 
+        if (!projectId) {
+            throw new ApiError(400, "Project id not found");
+        }
+
+        if (!isValidObjectId(projectId)) {
+            throw new ApiError(400, "Invalid project id");
+        }
+
+        const project = await Project.findOne({
+            _id: projectId,
+            workspaceId: req.workspace._id,
+        });
+
+        if (!project) {
+            throw new ApiError(404, "Project not found");
+        }
+
+        req.project = project;
+
+        next();
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid request");
+    }
 });
 
 export { verifyProjectInWorkspace };
