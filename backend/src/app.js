868907008f861853c057679ee36f5531eac4f9f4
hashirkhaaan <grammarkgl123@gmail.com>
2026-08-23 13:28:54 +0500

    Added and setup router and routes declerations

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import passport from "./config/passport.js"
-import session from "express-session"
+import passport from "./config/passport.js";
+import session from "express-session";
 
 const app = express();
 
@@ -20,8 +20,8 @@ app.use(
         resave: false,
         saveUninitialized: false,
     })
-)
-app.use(passport.initialize())
+);
+app.use(passport.initialize());
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
@@ -29,10 +29,12 @@ app.use(cookieParser());
 
 // routes import
 import userRouter from "./routes/user.routes.js";
-import authRouter from "./routes/auth.routes.js"
+import authRouter from "./routes/auth.routes.js";
+import workspaceRouter from "./routes/workspace.routes.js";
 
 // routes decleration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/workspace");
 
 export { app };
