b29db185d85d17090f680eb7c50b87954fae3595
hashirkhaaan <grammarkgl123@gmail.com>
2026-08-24 16:45:17 +0500

    Added, setup and updated routes for user controller

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,19 +1,31 @@
 import { Router } from "express";
-import { upload } from "../middlewares/multer.middlware.js";
+import {
+    uploadAvatar,
+    validateFileSignatures,
+} from "../middlewares/multer.middlware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
     getCurrentUser,
-    loginUser,
-    logoutUser,
-    refreshAccessToken,
-    registerUser,
+    changeCurrentPassword,
+    updateUserAvatarImage,
+    updateAccountDetails,
 } from "../controllers/user.controller.js";
-import { verifyJWT } from "../middlewares/auth.middleware.js";
+
 const router = Router();
 
-
 // secure routes
-router.route("/current-user").get(verifyJWT, getCurrentUser)
 
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router
+    .route("/update-avatar")
+    .patch(
+        verifyJWT,
+        uploadAvatar.single("avatar"),
+        validateFileSignatures,
+        updateUserAvatarImage
+    );
 
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 
 export default router;
